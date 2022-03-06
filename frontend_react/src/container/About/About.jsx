18b99a9f5755ada01d4a10afac722a4fca60e884
Ananthnbhat <ananthnbhat2@gmail.com>
2022-03-06 12:04:33 +1100

    complete about section

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,8 +1,71 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
+import { images } from "../../constants";
+
 import "./About.scss";
+import { urlFor, client } from "../../client";
+
+const abouts = [
+  {
+    title: "Web Development",
+    description: "I am a good web developer",
+    imgUrl: images.about01,
+  },
+  {
+    title: "Web Development",
+    description: "I am a good web developer",
+    imgUrl: images.about02,
+  },
+  {
+    title: "Web Development",
+    description: "I am a good web developer",
+    imgUrl: images.about03,
+  },
+  {
+    title: "Web Development",
+    description: "I am a good web developer",
+    imgUrl: images.about04,
+  },
+];
 
 function About(props) {
-  return <div>About</div>;
+  const [abouts, setAbouts] = useState([]);
+  useEffect(() => {
+    const query = '*[_type=="abouts"]';
+    client.fetch(query).then((data) => setAbouts(data));
+  }, []);
+
+  return (
+    <>
+      <h2 className="head-text">
+        I know that <span>Good development</span>
+        <br />
+        means <span>Good business</span>
+      </h2>
+
+      <div className="app__profiles">
+        {abouts.map((about, index) => {
+          return (
+            <motion.div
+              whileInView={{ opacity: 1 }}
+              whileHover={{ scale: 1.1 }}
+              transition={{ duration: 0.5, type: "tween" }}
+              className="app__profile-item"
+              key={about.title + index}
+            >
+              <img src={urlFor(about.imgUrl)} alt={about.title} />
+              <h2 className="bold-text" style={{ marginTop: 20 }}>
+                {about.title}
+              </h2>
+              <p className="p-text" style={{ marginTop: 10 }}>
+                {about.description}
+              </p>
+            </motion.div>
+          );
+        })}
+      </div>
+    </>
+  );
 }
 
 export default About;
